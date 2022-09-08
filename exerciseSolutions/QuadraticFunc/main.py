2782c95378ff5418803e02611b628de8b04060f9
TheFlyingPiano99 <57034327+TheFlyingPiano99@users.noreply.github.com>
2022-09-08 12:56:25 +0200

    Added if statement

--- exerciseSolutions/QuadraticFunc/main.py
+++ exerciseSolutions/QuadraticFunc/main.py
@@ -1,19 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def quadratic(x):
     a = 10
     b = 9
     c = 8
-    y = a * x**2 + b * x + c
+    y = a * x ** 2 + b * x + c
     print(f'F(x) = {y}')
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     x = float(input('Add meg az x értékét: '))
     quadratic(x)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
